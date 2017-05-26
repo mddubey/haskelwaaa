fedf784216632ab0e35d870345114c6fb0a80800
MD Dubey <mddubey409@gmail.com>
2017-05-26 10:40:50 +0530

    Minesweeperwaa thoda sa achchaa.. not working thik se.

--- 07_minesweeper.hs
+++ 07_minesweeper.hs
@@ -1,29 +1,69 @@
 import System.IO
+import Data.List
+import Debug.Trace
 
-data Square = Mine | Okey deriving Show
+data Square = OPEN | CLOSE Int | FLAG deriving Show
 
-grid = [[Mine,Okey,Okey],[Okey,Mine,Mine],[Okey,Mine,Okey]]
+grid = [[CLOSE 1,CLOSE 0,CLOSE 1]
+      ,[CLOSE 0,CLOSE 1,CLOSE 0]
+      ,[CLOSE 1,CLOSE 0 ,CLOSE 0]]
 
+--check whether or not game is over
+check OPEN _ = True
+check FLAG _ = True
+check (CLOSE 1) "O" = False
+check (CLOSE 1) "F" = True
+check (CLOSE 0) "O" = True
+check (CLOSE 0) "F" = True
+
+replace row y newSquare = do
+  let (c,_:cs) = splitAt y row
+  c ++ newSquare : cs
+
+--apply the changes on grid
+apply grid x y "O" = do
+  let newRow = replace (grid !! 1) y OPEN
+  let (c,_:cs) = splitAt x grid
+  c ++ newRow : cs
+
+apply grid x y "F" = do
+  let newRow = replace (grid !! 1) y FLAG
+  let (c,_:cs) = splitAt x grid
+  c ++ newRow : cs
+  
 prompt :: String -> IO String
 prompt text = do
     putStr text
     hFlush stdout
     getLine
 
-check Mine = print $ "Game Over"
-check Okey = do
-	print $ "It's not a Mine. Continue"
-  	input <- prompt "Enter the x coordinate:"
-  	let x = read input :: Int
+--display the grid
 
-  	input <- prompt "Enter the y coordinate:"
-  	let y = read input :: Int
-  	check (grid !! x !! y)
+map_square (OPEN) = "O"
+map_square (CLOSE _) = "C"
+map_square (FLAG) = "F"
 
-main = do
-    input <- prompt "Enter the x coordinate:"  
-    let x = read input :: Int
+display_line line = map map_square line
+display_grid grid = mapM_ putStrLn ((map (intercalate ", ") (map display_line grid)))
 
-    input <- prompt "Enter the y coordinate:"  
-    let y = read input :: Int
-    print $ check (grid !! x !! y)
+start grid = do
+  print $ grid
+  print $ "Here is your grid"
+  display_grid grid
+
+  input <- prompt "Enter the x coordinate:"  
+  let x = read input :: Int
+
+  input <- prompt "Enter the y coordinate:"  
+  let y = read input :: Int
+
+  input <- prompt "Enter the action:"  
+    
+  if not (check (grid !! x !! y) input) 
+    then print $ "Game Over"
+    else do 
+      let grid = apply grid x y input
+      start grid
+
+
+main = start grid
